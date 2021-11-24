19ebe33cc9e2a0c15ef0e5194bdf4160d4b22dc9
Caliche Orozco <caliche2da1@gmail.com>
2021-11-23 18:08:31 -0600

    se quitaron elementos extras

--- src/components/University.js
+++ src/components/University.js
@@ -16,7 +16,6 @@ function University(props) {
           src="https://images.unsplash.com/photo-1632616427546-a471c1cff36a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
         />
         <Card.Body>
-          {/* Se coloca el title del bookmark que esta en la posición 1 del vector */}
           <Card.Title>{props.uni.name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             {props.uni.web_pages}
